43d66361323c08af395d38952cd98aa65162c58b
bd0remus <breeann.doremus@spaceforce.mil>
2024-11-18 17:21:52 -0500

    Add files via upload

---
+++ knex-database-codeAlong/knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://bd0remus:Be@nS371521!!@localhost/pg-dockercd '
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
